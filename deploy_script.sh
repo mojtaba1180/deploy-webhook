988261509cb4096ba88bcfdfd4362a1c6198096e
mojtaba-1180 <mohammadsgrdev@gmail.com>
2024-06-21 15:46:13 +0330

    update

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-
 update_project() {
-    echo "Updating project in $1..."
-    cd "$1" || exit
+    local project_path="$1"
+    echo "Updating project in $project_path..."
+    cd "$project_path" || exit
 
-    #
     log_file="/home/nfs/salmej/runner/logs/docker_log-$(date +%Y-%m-%d-%H-%M-%S).txt"
 
+    # Execute docker commands separately and log output
+    docker compose down | tee -a "$log_file"
+    docker compose up --build -d | tee -a "$log_file"
 
-    (docker compose down && docker compose up --build -d) | tee -a "$log_file"
     echo "Project updated."
 }
 
-
+# Array of project paths
 declare -a paths=(
     "/home/nfs/salmej/salmej-api/www"
     "/home/nfs/salmej/salmej-dashboard-ui/www"
 )
 
-
+# Loop through each path to check for updates
 for path in "${paths[@]}"; do
     echo "Checking for updates in $path..."
     cd "$path" || exit
@@ -29,7 +30,7 @@ for path in "${paths[@]}"; do
     git_output=$(git pull | tee -a "$log_file")
     
     if [[ "$git_output" != *"Already up to date."* ]]; then
-        update_project "/home/nfs/salmej"
+        update_project "$path"
     else
         echo "No updates needed for $path."
     fi
